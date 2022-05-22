6740ba2e17a739a28ce35780f3eccdbce7eb2956
Rishabh Mishra <rishabhmishra757@gmail.com>
2022-05-22 12:40:09 +0530

    first commit

---
+++ micro-front-end-one/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+  constructor(private injector: Injector) {
+    const myCustomElement = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('app-mfe-one', myCustomElement);
+  }
+}
